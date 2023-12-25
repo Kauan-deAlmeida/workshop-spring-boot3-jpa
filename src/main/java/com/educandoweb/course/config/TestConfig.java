d1f0a5709966ac8b41660b46081cad36a18d4102
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-12-25 02:47:37 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -23,7 +23,5 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
-	}
-	
-		
+	}	
 }
